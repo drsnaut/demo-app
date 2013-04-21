62b40f6523ba0b39a918f1233624dd30e1261dfa
LFCA <lfca.cpr@gmail.com>
2013-04-21 12:38:55 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cc635075642433803b40c4d4397e7d22499197a9e59e427c055349671d001914e6af3d899a235bd283e1e7b0d7e18ac82bd1900ebccc9547a925b449b73c28d7'
